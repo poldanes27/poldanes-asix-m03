6a88e0c6343980af304267ee27d66982919c095a
pol.danes.7e7 <pol.danes.7e7@itb.cat>
2023-09-28 13:00:49 +0200

    hola

---
+++ main.py
@@ -0,0 +1,6 @@
+import math
+
+diametre = float(input("Introdueix el diametre de la pizza: "))
+radio = diametre / 2
+superficie = math.pi * (radio ** 2)
+print("La superficie de la pizza es: ", superficie )
