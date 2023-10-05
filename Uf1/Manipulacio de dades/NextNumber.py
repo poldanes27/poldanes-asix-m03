23135bdff549560db7ae1e1648c9cdb57d1ca9f8
pol.danes.7e7 <pol.danes.7e7@itb.cat>
2023-10-05 12:02:27 +0200

    pol danes

---
+++ Uf1/Manipulacio de dades/NextNumber.py
@@ -0,0 +1,6 @@
+# L'usuari escriu un número enter. Printa per pantalla el número seguent
+# Per exemple si entra 4 el programa ha de mostrar la frase despres ve el 5
+
+numero = int(input("Introdueix un numeor enter: "))
+numero = numero+1
+print ("Despres ve el: " ,numero )
